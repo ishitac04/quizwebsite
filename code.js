4a2383dec7f976f55d4834763f8a691a6c8d7d18
ishitac04 <ishitaa.chowdhury@gmail.com>
2025-08-30 22:11:00 +0100

    added questions and answers

--- code.js
+++ code.js
@@ -1,38 +1,45 @@
-let currentQ = 0;
 const boxes = document.querySelectorAll(".box");
 const next = document.querySelector(".nextbutton");
 const correct = document.getElementById('correct');
 const quizname = document.querySelector(".quizname");
 const question = document.querySelector(".titlewrapper");
+const currentanswers = []
+let questionNumber;
 
 const questions = [
     "How many dots are on a pair of dice?", "What country has the highest life expectancy?","Who is the ancient Greek god of the Sun?","What is the capital of Spain?","When did WW1 start?"
 ]
 const answers = [
-    "Answer1","Answer2","Answer3","Answer4",
-    "Answer5","Answer6","Answer7","Answer8",
-    "Answer9","Answer10","Answer11","Answer12"
+    "21","20","24","42",
+    "Japan","Mexico","Hong Kong","Tunisia",
+    "Zeus","Poseidon","Ares","Apollo",
+    "Valencia","Madrid","Barcelona","Tenerife",
+    "1915","1914","1934","1922"
 ]
-
 function generateQuestion() {
-    let questionNumber=Math.floor((Math.random()/2)*10);
+    questionNumber = Math.floor(Math.random() * questions.length);
+    question.textContent = questions[questionNumber];
+    const n = questionNumber * 4;
+    const currentanswers = answers.slice(n, n + 4);
+
     boxes.forEach((box, i) => {
-        next.addEventListener("click", () => {
-            if (questionNumber==0) {
-                question.textContent=questions[questionNumber];
-            } else if (questionNumber==1) {
-                question.textContent=questions[questionNumber];
-            } else if (questionNumber==2) {
-                question.textContent=questions[questionNumber];
-            } else if (questionNumber==3) {
-                question.textContent=questions[questionNumber];
-            } else if (questionNumber==4) {
-                question.textContent=questions[questionNumber];
-            }
-        });
+        box.textContent = currentanswers[i];
     });
 }
 
-function myFunction() {
-    generateQuestion();
+next.addEventListener("click", generateQuestion);
+
+generateQuestion();
+
+function generateAnswers() {
+    for (let a = 0; a < answers.length; a++) {
+        n=questionNumber*4;
+        console.log(n)
+        if (n<=a && a<n+4) {
+            console.log("In loop")
+            currentanswers.push(answers[a]);
+            console.log(currentanswers);
+        }
+    }
+    currentanswers=[]
 }
